751b6d178430072dc8480a4305bb9207d6a11727
Lynh <jenky.w0w@gmail.com>
2023-07-24 10:12:57 +0700

    Remove trait

--- tests/NullPool.php
+++ tests/NullPool.php
@@ -5,17 +5,23 @@ declare(strict_types=1);
 namespace Jenky\Concurrency\Tests;
 
 use Jenky\Concurrency\PoolInterface;
-use Jenky\Concurrency\PoolTrait;
 
 final class NullPool implements PoolInterface
 {
-    use PoolTrait;
-
     private $responses = [];
 
     public function __construct(array $responses = [])
     {
         $this->responses = $responses;
+    }
+
+    public function concurrent(int $concurrency): PoolInterface
+    {
+        if ($concurrency < 1) {
+            throw new \ValueError('Argument #1 ($concurrency) must be positive, got '.$concurrency);
+        }
+
+        return clone $this;
     }
 
     public function send(iterable $requests): array
